7acc4d8cb5f8b8c8e9827c2fb5015fb4e653f52d
Tommaso Nicosia <tommaso.nicosia@enea.it>
2016-08-31 15:08:23 +0200

    ready for test and debug, not for deploy

--- config.py
+++ config.py
@@ -20,5 +20,5 @@ OPERATION="append"
 HPCSESSIONID="4f5abbf08000cf9b9948dbf42c003415"
 URL=POSTSERVER+"/"+POSTDB+"/_design/"+HANDLE+"/_update/"+OPERATION+"/"+HPCSESSIONID
 WORKDIR="{0}/fermi/.".format(os.environ.get("HOME"))
-COMMANDPOSTURL=POSTSERVER+"/commands/_design/"+HANDLE+"/_update/"+OPERATION+"/"+HPCSESSION
+COMMANDPOSTURL=POSTSERVER+"/commands/_design/"+HANDLE+"/_update/"+OPERATION+"/"+HPCSESSIONID
 DEFAULTFOLDER="webfolder"
